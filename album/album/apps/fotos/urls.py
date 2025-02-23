34352472b9475a49647063bdf67ca534b7c97a7b
Nicolas Torres <111529835+Thowers@users.noreply.github.com>
2025-02-22 19:49:06 -0500

    Delete album/album directory

--- album/album/apps/fotos/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [ 
-    path("visor", views.fotos, name="fotos"),
-    + path('fotos/', include('apps.fotos.urls')),  # Incluye las rutas de la app fotos
-]
